1fa0c1d3a41ff861059a4616f142c0f62a1b527e
Andrew Diamond <axd1997@gmail.com>
2017-04-22 14:44:43 -0400

    Update packages, add OAuth

--- Samaritans/Samaritans/App_Start/RouteConfig.cs
+++ Samaritans/Samaritans/App_Start/RouteConfig.cs
@@ -18,6 +18,9 @@ namespace Samaritans
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(name: "signin-google", url: "signin-google", defaults: new { controller = "Account", action = "ExternalLoginCallback" });
+
         }
     }
 }
--- Samaritans/Samaritans/Controllers/HomeController.cs
+++ Samaritans/Samaritans/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Samaritans.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
