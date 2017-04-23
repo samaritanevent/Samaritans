745d4cec11557b588ea018ced68fa74fbb5d7578
JMoor <JMoor@HERMES>
2017-04-23 15:57:38 -0400

    Removed a relationship to let us join events.

--- Samaritans/Samaritans.Data/Entities/Attendee.cs
+++ Samaritans/Samaritans.Data/Entities/Attendee.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Samaritans.Data.Entities
+﻿namespace Samaritans.Data.Entities
 {
     public class Attendee
     {
@@ -9,15 +7,12 @@ namespace Samaritans.Data.Entities
         public string UserId { get; set; }
 
         public int EventId { get; set; }
-
-        public int ResourceId { get; set; }
-
+        
         public decimal Quantity { get; set; }
 
         public virtual AspNetUser User { get; set; }
 
         public virtual Event Event { get; set; }
-
-        public virtual Resource Resource { get; set; }
+        
     }
 }
